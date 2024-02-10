a60b65a0a53bcf72d705a64b73803d94a7a3e61f
rabbi-official <golam_rabbi@coredevs.ltd>
2024-02-11 03:42:02 +0600

    problem 1 completed

--- src/components/Problem-1.jsx
+++ src/components/Problem-1.jsx
@@ -3,9 +3,66 @@ import React, {useState} from 'react';
 const Problem1 = () => {
 
     const [show, setShow] = useState('all');
+    //const [active,setActive] = useState({});
+    // const [completed,setCompleted] = useState([]);
+    const [data,setData] = useState([]);
 
+    const dataObj = {};
+
+    const handleAddTask = (e) =>{
+        e.preventDefault();
+        const taskName = e.target.name.value;
+        const status = e.target.status.value;
+        
+        if(status === 'Active' || status === 'active'){
+            let myArray = JSON.parse(localStorage.getItem('Active')) || [];
+            myArray.push(taskName);
+            const updatedArray = JSON.stringify(myArray);
+            localStorage.setItem('Active',updatedArray);
+        }
+
+        if(status === 'Completed' || status === 'completed'){
+            let myArray = JSON.parse(localStorage.getItem('Completed')) || [];
+            myArray.push(taskName);
+            const updatedArray = JSON.stringify(myArray);
+            localStorage.setItem('Completed',updatedArray);
+        }
+        else{
+            let myArray = JSON.parse(localStorage.getItem(status)) || [];
+            myArray.push(taskName);
+            const updatedArray = JSON.stringify(myArray);
+            localStorage.setItem(status,updatedArray);
+        }
+
+        
+    }
     const handleClick = (val) =>{
         setShow(val);
+        if(val === 'active'){
+          const activeObj =  JSON.parse(localStorage.getItem('Active'));
+          setData(Object.values(activeObj));
+        }
+        if(val === 'completed'){
+            const completedObj =  JSON.parse(localStorage.getItem('Completed'));
+            setData(Object.values(completedObj));  
+        }
+
+        if(val === 'all'){
+            Object.keys(localStorage).forEach(key => {
+              const allObj = JSON.parse(localStorage.getItem(key));
+              const combinedArray = [].concat(...Object.values(allObj));
+              setData(combinedArray);
+              //console.log(x);
+              //console.log(typeof Object.values(allObj));
+              //setData(Object.values(allObj));
+            });
+            
+        }
+
+
+       
+
+
     }
 
     return (
@@ -14,12 +71,12 @@ const Problem1 = () => {
             <div className="row justify-content-center mt-5">
                 <h4 className='text-center text-uppercase mb-5'>Problem-1</h4>
                 <div className="col-6 ">
-                    <form className="row gy-2 gx-3 align-items-center mb-4">
+                    <form className="row gy-2 gx-3 align-items-center mb-4" onSubmit={handleAddTask}>
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Name"/>
+                            <input type="text" name='name' className="form-control" placeholder="Name"/>
                         </div>
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Status"/>
+                            <input type="text" name='status' className="form-control" placeholder="Status"/>
                         </div>
                         <div className="col-auto">
                             <button type="submit" className="btn btn-primary">Submit</button>
@@ -46,8 +103,18 @@ const Problem1 = () => {
                             <th scope="col">Status</th>
                         </tr>
                         </thead>
-                        <tbody>
                         
+                        <tbody>
+                    
+                         {data.map((element, index) => (
+                          <tr key={index}>
+                            <td>{element}</td>
+                            <td>{show}</td>
+                             
+                         </tr>
+                        ))}
+
+                    
                         </tbody>
                     </table>
                 </div>
